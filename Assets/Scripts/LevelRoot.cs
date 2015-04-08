d94f0482b4d79ecb77d099801280ea63a8f6c2f5
Andrew Jones <ajon542@gmail.com>
2015-04-08 07:38:12 -0700

    Initial commit of level generator

---
+++ Assets/Scripts/LevelRoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRoot : MonoBehaviour
+{
+    public Room room;
+
+    private List<Room> rooms; 
+
+    private void Start()
+    {
+        rooms = new List<Room>();
+        rooms.Add(CreateRoom(new Vector2(0, 0), 3, 7));
+        rooms.Add(CreateRoom(new Vector2(10, 10), 3, 4));
+    }
+
+    private Room CreateRoom(Vector2 position, int width, int length)
+    {
+        Room obj = Instantiate(room, new Vector3(0, 0, 0), Quaternion.identity) as Room;
+        obj.GenerateRoom(position, width, length);
+        return obj;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject floorTile;
+
+    private GameObject[,] floorArea;
+
+    public int Width { get; private set; }
+    public int Length { get; private set; }
+
+    public void GenerateRoom(Vector2 position, int width, int length)
+    {
+        Width = width;
+        Length = length;
+
+        floorArea = new GameObject[width, length];
+
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < length; ++j)
+            {
+                floorArea[i, j] = Instantiate(floorTile, new Vector3(position.x + i, 0, position.y + j), new Quaternion(1, 0, 0, 1)) as GameObject;
+            }
+        }
+    }
+}
