8f0d01773ae13469bac4c60df390299c38055e1d
Andrew Jones <ajon542@gmail.com>
2015-04-08 15:53:14 -0700

    Added the north and the east walls

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// A room contains the floor and walls.
@@ -11,9 +12,24 @@ public class Room : MonoBehaviour
     public GameObject floorTile;
 
     /// <summary>
+    /// The prefab used for the walls.
+    /// </summary>
+    public GameObject wall;
+
+    /// <summary>
     /// The array storing the total floor area.
     /// </summary>
     private GameObject[,] floorArea;
+
+    /// <summary>
+    /// The north wall of the room.
+    /// </summary>
+    private List<GameObject> northWall;
+
+    /// <summary>
+    /// The east wall of the room.
+    /// </summary>
+    private List<GameObject> eastWall;
 
     /// <summary>
     /// Gets the width of the room.
@@ -33,7 +49,25 @@ public class Room : MonoBehaviour
     /// <param name="length">The length of the room.</param>
     public void GenerateRoom(Vector3 position, int width, int length)
     {
+        // NOTE: The south and west walls are not needed as they are viewed
+        // from behind and aren't rendered anyway.
         floorArea = new GameObject[width, length];
+        northWall = new List<GameObject>();
+        eastWall = new List<GameObject>();
+
+        // Generate north walls.
+        for (int i = 0; i < width; ++i)
+        {
+            GameObject obj = Instantiate(wall, new Vector3(position.x + i, position.y + 0.5f, position.z + length - 0.5f), Quaternion.identity) as GameObject;
+            northWall.Add(obj);
+        }
+
+        // Generate east walls.
+        for (int i = 0; i < length; ++i)
+        {
+            GameObject obj = Instantiate(wall, new Vector3(position.x + width - 0.5f, position.y + 0.5f, position.z + i), Quaternion.Euler(0, 90, 0)) as GameObject;
+            eastWall.Add(obj);
+        }
 
         // Generate a floor tile for each section of the floor.
         for (int i = 0; i < width; ++i)
