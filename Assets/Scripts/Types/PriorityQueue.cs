4ae946adc610d5885c3ffdff2dfce08e3d00f6e2
Andrew Jones <ajon542@gmail.com>
2015-04-13 16:21:46 -0700

    Added Enqueue and Dequeue methods to PriorityQueue

--- Assets/Scripts/Types/PriorityQueue.cs
+++ Assets/Scripts/Types/PriorityQueue.cs
@@ -19,14 +19,28 @@ public class PriorityQueue<T>
 
     public T Dequeue()
     {
-        // TODO:
-        // Get the highest priority key.
+        // Highest priority key should be at start of the list
         IList<int> keys = priorityQueue.Keys;
 
-        foreach (int key in keys)
+        if (keys.Count == 0)
         {
-            Debug.Log("Key: " + key);
+            throw new Exception("queue is empty");
         }
-        return default(T);
+
+        // Dequeue the item.
+        T item = priorityQueue[keys[0]].Dequeue();
+
+        // Remove the priority key if there are no more items to remove.
+        if (priorityQueue[keys[0]].Count == 0)
+        {
+            priorityQueue.Remove(keys[0]);
+        }
+
+        return item;
+    }
+
+    public int Count
+    {
+        get { return priorityQueue.Count; }
     }
 }
