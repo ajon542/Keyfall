dfc65cc18290d1fa6a643c44a97cb0215195e02e
Andrew Jones <ajon542@gmail.com>
2016-02-22 20:07:21 -0800

    Added town and store scripts

---
+++ Assets/Scripts/Town.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Town : MonoBehaviour
+{
+    // House - storage of items
+    // General Store - food, water, torches, medical supplies
+    // Armour - legs, body, arms, helmets, boots
+    // Weapons - swords, daggers, spears, axes
+    // Magic / Alchemy - potions, spells, wands, teleportation devices
+    // Library - books for learning about various things
+    // Black market - rare items, rings, amulets
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
