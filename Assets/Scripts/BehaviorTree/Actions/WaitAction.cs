08246d36ea08c5848465877cbbb3838dd7a78f43
Andrew Jones <andrew.jones@tapslots.com>
2019-02-23 13:09:08 -0800

    Added scene to test behavior tree in action

---
+++ Assets/Scripts/BehaviorTree/Actions/WaitAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WaitAction : ITask
+{
+    private float _seconds;
+    private float _currentTime;
+    
+    public WaitAction(float seconds)
+    {
+        _seconds = seconds;
+    }
+
+    public TaskStatus Tick()
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= _seconds)
+            return TaskStatus.Success;
+        
+        return TaskStatus.Incomplete;
+    }
+}
---
+++ Assets/Scripts/TestBehaviorTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviorTree : MonoBehaviour
+{
+    private BehaviourTree _behaviorTree;
+    void Start()
+    {
+        var sequenceTask = new SequenceTask(new List<ITask>
+        {
+            new LogAction("Child 1"),
+            new WaitAction(1),
+            new LogAction("Child 2"),
+            new WaitAction(1),
+            new LogAction("Child 3"),
+            new LogAction("Child 4"),
+            new LogAction("Child 5"),
+            new WaitAction(1),
+            new LogAction("Child 6"),
+            new LogAction("Child 7"),
+            new LogAction("Child 8"),
+            new WaitAction(1),
+            new LogAction("Child 9"),
+        });
+        _behaviorTree = new BehaviourTree(sequenceTask);
+    }
+
+    private void Update()
+    {
+        if (_behaviorTree.CurrentStatus == TaskStatus.Incomplete)
+            _behaviorTree.Tick();
+    }
+}
